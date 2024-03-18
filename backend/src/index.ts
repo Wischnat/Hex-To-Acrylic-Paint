bb3560cb9716d5c2f41462f300db5d08f51d72a3
Julian Wischnat <julian.wischnat@googlemail.com>
2024-03-18 17:42:07 +0100

    feat: adds compare paint route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { logger } from "./utils/";
 import http from "http";
-import { EntityManager, EntityRepository, MikroORM } from "@mikro-orm/core";
+import {
+  EntityManager,
+  EntityRepository,
+  MikroORM,
+  RequestContext,
+} from "@mikro-orm/core";
 import { Paint } from "./entities";
+import { PaintController } from "./controller";
 
-const PORT = 4000;
+const PORT: number = 4000;
 const app = express();
 
 export const DI = {} as {
@@ -19,6 +25,19 @@ export const initializeServer = async () => {
   DI.em = DI.orm.em;
   DI.paintRepository = DI.orm.em.getRepository(Paint);
 
+  app.use(express.json());
+  app.use((req: Request, res: Response, next: NextFunction) =>
+    RequestContext.create(DI.orm.em, next)
+  );
+
+  app.use("/paint", PaintController);
+
+  app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+    res.status(500).json({
+      error: err.message,
+    });
+  });
+
   app.listen(PORT, () => {
     logger
       .child({ context: { event: "startServer" } })
@@ -27,3 +46,6 @@ export const initializeServer = async () => {
 };
 
 initializeServer();
+
+// https://reflectoring.io/express-error-handling/
+// https://stackoverflow.com/questions/68463549/express-try-and-catch-in-form-of-middleware
