409cca5116d9cbac39eb147b314c2ea60b223bcd
Julian Wischnat <julian.wischnat@googlemail.com>
2024-04-02 15:01:32 +0200

    rename route

--- backend/src/controller/paint.controller.ts
+++ backend/src/controller/paint.controller.ts
@@ -37,7 +37,7 @@ const compareAllPaints = (
 };
 
 router.get(
-  "/compare",
+  "/findEquivalent",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const hexColor: string = req.query.hexColor as string;
